d0f25cd201d59e5f60e206c6e5effb4a4ab058dc
tashfeenrao <raotashfeen25@gmail.com>
2020-05-15 19:40:33 +0500

    removed un used files from framwork

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 import './style.scss';
-import './js/bin/materialize.min';
-import './sass/materialize.scss';
-import getData from './getWeather';
 import domListner from './domListner';
 
 domListner.getCityName();
-
-/* getData.sendRequest().then((object) => {
-console.log(object);
-}); */
